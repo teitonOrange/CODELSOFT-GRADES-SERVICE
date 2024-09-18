488c11d70a969d131049c4e1f7d3c43bb17c0319
teitonOrange <nicolas.gonzalez@alumnos.ucn.cl>
2024-09-18 16:52:35 -0300

    feat: new dto validators

--- src/grades/dto/create-grade.dto.ts
+++ src/grades/dto/create-grade.dto.ts
@@ -1,8 +1,27 @@
+import { IsString, IsNotEmpty, IsNumber, Min, Max, IsUUID } from "class-validator";
+
 export class CreateGradeDto {
+
+    @IsString()
+    @IsNotEmpty()
     public subjectName: string;
+
+    @IsNumber()
+    @Min(1)
+    @Max(7)
     public grade: number;
+
+    @IsString()
+    @IsNotEmpty()
     public gradeName: string;
+
+    @IsString()
+    @IsNotEmpty()
     public comment: string;
+
+    @IsUUID()
     public userId: string;
+
+    @IsUUID()
     public studentId: string;
 }
--- src/grades/dto/update-grade.dto.ts
+++ src/grades/dto/update-grade.dto.ts
@@ -1,7 +1,22 @@
+import { IsInt, IsNotEmpty, isNotEmpty, IsNumber, IsString, Max, Min } from "class-validator";
+
 export class UpdateGradeDto {
+
+    @IsString()
+    @IsNotEmpty()
     public subjectName: string;
+
+    @IsNumber()
+    @Min(1)
+    @Max(7)
     public grade: number;
+
+    @IsString()
+    @IsNotEmpty()
     public gradeName: string;
+
+    @IsString()
+    @IsNotEmpty()
     public comment: string;
 
 }
