3e65c17baedefd64f1f305941b815b55ba97d9d6
teitonOrange <nicolas.gonzalez@alumnos.ucn.cl>
2024-09-18 17:56:23 -0300

    feat: remove example endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { GradesModule } from './grades/grades.module';
 
 @Module({
   imports: [GradesModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
